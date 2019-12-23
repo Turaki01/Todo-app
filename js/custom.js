ab3ead2a87e8ae03916dd72e485de1aee954f2f9
Turaki01 <ezekiel_Zakari@yahoo.com>
2019-12-23 01:50:50 +0100

    added search filter

--- js/custom.js
+++ js/custom.js
@@ -31,13 +31,21 @@ const filter = {
 
 
 // Function to render todo
-const renderTodo = function (todo) {
+const renderTodo = function (todos, filter) {
 
-    document.querySelector('#todo-list').innerHTML = ''
+    const filterTodo = todos.filter((todo) => {
+        return todo.text.toLowerCase().includes(filter.searchText.toLowerCase());
+    });
+
+
+
+    document.querySelector('#todo-list').innerHTML = '';
+
+    document.querySelector('#todo-summary').innerHTML = '';
 
     
 
-    const incompletedTodo = todo.filter((todo) => {
+    const incompletedTodo = filterTodo.filter((todo) => {
         return !todo.completed
     });
 
@@ -45,7 +53,7 @@ const renderTodo = function (todo) {
     summary.textContent = `You have ${incompletedTodo.length} todos left`;
     document.querySelector('#todo-summary').appendChild(summary);
 
-    todo.forEach((todo, index) => {
+    filterTodo.forEach((todo, index) => {
 
         // adding todos to the DOM
         const todoEl = document.createElement('p');
@@ -59,12 +67,13 @@ const renderTodo = function (todo) {
 
 }
 
-renderTodo(todos);
+renderTodo(todos, filter);
 
 
 // Input event on the input field to filter todo
 document.querySelector('#searchText').addEventListener('input', (e) => {
     filter.searchText = e.target.value;
+    renderTodo(todos, filter)
 });
 
 // submit event on the add todo form
@@ -76,7 +85,7 @@ document.querySelector('#addTodoForm').addEventListener('submit', (e) => {
         completed: false
     })
     
-    renderTodo(todos);
+    renderTodo(todos, filter);
 
     e.target.addTodo.value = '';
 });
